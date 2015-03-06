0891201044594336e07e1fc28799f5da32b47c60
ringeringering <ringeringg@gmail.com>
2015-03-05 18:49:17 -0800

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -1,9 +1,47 @@
+#include <ctime>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+/*void marble_sort(myArray[]);
+{
+  
+}*/
+
+void readFile(string myArray[])
+{
+    ifstream file ("marbles.txt");
+    if(file.is_open())
+    {
+        int i = 0;
+
+        while(file.good() && i<2000)
+        {
+            myArray[i] = file.get();
+            i++;
+        }
+    }
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    string myArray[2000];
+    readFile(myArray);
+
+    /*for(int j = 0; j<2000 ; j++)
+    cout << myArray[j];*/
+
+    clock_t t1, t2;
+    // Input and initializations occur here before the clock starts.
+    t1 = clock();
+    for(long int i=1; i <= 1000000; i++) // Loopp only if using clock()
+    {
+        marble_sort(myArray)
+    }
+    t2 = clock();
+    // Output for verification occurs here after the clock stops.
+    cout<< "Time difference is " << (t2-t1)/CLK_TCK << " microseconds";
     return 0;
 }
