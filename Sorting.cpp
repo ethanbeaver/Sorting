2d2fd417450119c3705222d05ed5a3c86fb88b88
ethanbeaver <ethanbeaver35@gmail.com>
2015-03-13 00:33:25 -0700

    Update Sorting.cpp

--- Sorting.cpp
+++ Sorting.cpp
@@ -10,7 +10,6 @@ void readFile(char myArray[])
     if(file.is_open())
     {
         int i = 0;
-
         while(file.good() && i<2000)
         {
             myArray[i] = file.get();
@@ -19,56 +18,46 @@ void readFile(char myArray[])
     }
 }
 
-void marble_sort(char myArray[])
-{
-    int i, j, n, t;
-    i = j = 0;
-    n = 1999;
-
-    while (j <= n)
-    {
-        if (myArray[j] == 'R')
-        {
-            t = myArray[i];
-            myArray[i] = myArray[j];
-            myArray[j] = t;
-            i++;
-            j++;
-        }
-        else if (myArray[j] == 'B')
-            {
-                t = myArray[j];
-                myArray[j] = myArray[n];
-                myArray[n] = t;
-                n--;
-            }
-            else if (myArray[j] == 'W')
-                {
-                    j++;
-                }
-    }
-}
-
 int main()
 {
 
     char myArray[2000];
     readFile(myArray);
-
-    /*for(int j = 0; j<2000 ; j++)
-    cout << myArray[j];*/
-
+    int i, j, n, t;
 
     clock_t t1, t2;
     // Input and initializations occur here before the clock starts.
     t1 = clock();
-    for(long int i=1; i <= 1000000; i++) // Loop only if using clock()
+    for(long int q=1; q <= 1000000; q++) // Loop only if using clock()
     {
-        marble_sort(myArray);
+        i = j = 0;
+        n = 1999;
+        while (j <= n)
+        {
+            if (myArray[j] == 'R')
+            {
+                t = myArray[i];
+                myArray[i] = myArray[j];
+                myArray[j] = t;
+                i++;
+                j++;
+            }
+            else if (myArray[j] == 'B')
+                {
+                    t = myArray[j];
+                    myArray[j] = myArray[n];
+                    myArray[n] = t;
+                    n--;
+                }
+                else if (myArray[j] == 'W')
+                    {
+                        j++;
+                    }
+        }
     }
     t2 = clock();
     // Output for verification occurs here after the clock stops.
     cout << myArray;
-    cout<< "Time difference is " << (t2-t1)/CLK_TCK << " microseconds";
+    cout<< "Time difference is " << (t2-t1)/CLOCKS_PER_SEC << " microseconds";
     return 0;
 }
